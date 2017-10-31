e3ae41f393ab26f8ecb2ef6b9e96ddc50dd17038
redligot2009 <redligot2009.forreal@gmail.com>
2017-10-31 21:58:38 +0800

    Fixed some bugs regarding fade-to-black on mobile menu expansion

--- js/page.js
+++ js/page.js
@@ -26,11 +26,22 @@ $(".navbar-toggler").click(
 );
 $(document).click(function (event) {
     var clickover = $(event.target);
-    var $navbar = $(".navbar-collapse");               
+    var $navbar = $(".navbar-collapse");
     var _opened = $navbar.hasClass("show");
-    if (_opened === true && !clickover.hasClass("navbar-toggle")) {      
+    if (_opened === true && !clickover.hasClass("navbar-toggle")) 
+    {     
         $navbar.collapse('hide');
     }
+});
+$(window).resize(function()
+{
+    var $navbar = $(".navbar-collapse");
+	var screenWidth = $(window).outerWidth();
+	if(screenWidth >= 576)
+	{
+		$(".fader").removeClass("triggered");
+		$navbar.removeClass('show');
+	}
 });
 $(".fader").click(
 	function()
